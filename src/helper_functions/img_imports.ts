85a91b52dc2e63895e0d38de00d5c5759e3d88ff
Danglebary <halfblowncontact@gmail.com>
2021-08-18 00:24:47 -0700

    added genre list item images and created helper image import file

---
+++ src/helper_functions/img_imports.ts
@@ -0,0 +1,9 @@
+import pic_1 from '../imgs/genre_list_item_background_0004_1.jpg'
+import pic_2 from  '../imgs/genre_list_item_background_0003_2.jpg'
+import pic_3 from  '../imgs/genre_list_item_background_0002_3.jpg'
+import pic_4 from  '../imgs/genre_list_item_background_0001_4.jpg'
+import pic_5 from  '../imgs/genre_list_item_background_0000_5.jpg'
+
+
+
+export { pic_1, pic_2, pic_3, pic_4, pic_5 }
