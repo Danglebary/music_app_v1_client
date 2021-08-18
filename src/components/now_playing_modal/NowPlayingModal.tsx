edfad0eaa1848a9c8156ed7edb80d376f46232a3
Danglebary <halfblowncontact@gmail.com>
2021-08-18 00:30:26 -0700

    created files for "now playing" modal, no functionality or design yet

---
+++ src/components/now_playing_modal/NowPlayingModal.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+import './nowPlayingModalStyles.css';
+
+interface Props {}
+
+export default function NowPlayingModal({}: Props): ReactElement {
+    return <div></div>;
+}
